3e2997d1a00713cef4c1f4aaaae1bdae354abbf2
Jan Szewczyk <36161455+IvanPlease@users.noreply.github.com>
2021-10-23 19:30:50 +0200

    [Init] Commit

--- src/main/java/queivan/harcmiliada/service/repository/QuestionRepository.java
+++ src/main/java/queivan/harcmiliada/service/repository/QuestionRepository.java
@@ -1,6 +1,7 @@
 package queivan.harcmiliada.service.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import queivan.harcmiliada.domain.Question;
@@ -14,6 +15,7 @@ public interface QuestionRepository extends JpaRepository<Question, UUID> {
     @Query(nativeQuery = true, value = "SELECT * FROM _questions WHERE _questions.current = true")
     Optional<Question> findCurrent();
     @SuppressWarnings("SqlWithoutWhere")
+    @Modifying
     @Query(nativeQuery = true, value = "UPDATE _questions SET current = false")
     void clearCurrent();
 }
